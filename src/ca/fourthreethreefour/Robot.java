5d91d6c67831392e299506d272c8ded7cd811669
Kulkinz <cool@coolspages.com>
2018-01-04 16:05:55 -0700

    Controls done

---
+++ src/ca/fourthreethreefour/Robot.java
@@ -0,0 +1,66 @@
+package ca.fourthreethreefour;
+
+import ca.fourthreethreefour.commands.ReverseSolenoid;
+import edu.first.module.Module;
+import edu.first.module.actuators.DualActionSolenoid;
+import edu.first.module.actuators.DualActionSolenoid.Direction;
+import edu.first.module.actuators.DualActionSolenoidModule;
+import edu.first.module.joysticks.XboxController;
+import edu.first.module.joysticks.BindingJoystick.DualAxisBind;
+import edu.first.module.subsystems.Subsystem;
+import edu.first.robot.IterativeRobotAdapter;
+
+public class Robot extends IterativeRobotAdapter {
+
+	DualActionSolenoidModule armSolenoid = new DualActionSolenoidModule(0, 1);
+	
+	XboxController controller = new XboxController(0);
+	
+	private final Subsystem ALL_MODULES = new Subsystem(new Module[] { drive, armSolenoid, controller });
+	
+	public Robot() {
+		super("Steve");
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void init() {
+		ALL_MODULES.init();
+		
+		controller.addAxisBind(new DualAxisBind(controller.getLeftDistanceFromMiddle(), controller.getRightX()) {
+            @Override
+			public void doBind(double speed, double turn) {
+				if (turn == 0 && speed == 0) {
+					drivetrain.stopMotor();
+				} else {
+					drivetrain.arcadeDrive(speed, turn);
+				}				
+			}
+		});
+		
+		controller.addWhenPressed(XboxController.RIGHT_TRIGGER, new ReverseSolenoid(armSolenoid));
+	}
+	
+	@Override
+	public void initTeleoperated() {
+		ALL_MODULES.enable();
+		if (armSolenoid.get() == Direction.OFF) {
+			armSolenoid.set(DualActionSolenoid.Direction.LEFT);
+		}
+	}
+	
+	@Override
+	public void periodicTeleoperated() {
+		controller.doBinds();
+	}
+	
+	@Override
+	public void endTeleoperated() {
+		ALL_MODULES.disable();
+	}
+	
+	
+
+}
+
+	
---
+++ src/ca/fourthreethreefour/subsystems/Drive.java
@@ -0,0 +1,29 @@
+package ca.fourthreethreefour.subsystems;
+
+import edu.first.identifiers.Function;
+import edu.first.identifiers.InversedSpeedController;
+import edu.first.module.Module;
+import edu.first.module.actuators.Drivetrain;
+import edu.first.module.actuators.VictorModule;
+import edu.first.module.actuators.VictorModuleGroup;
+import edu.first.module.subsystems.Subsystem;
+
+public interface Drive {
+	
+	VictorModuleGroup left = new VictorModuleGroup(new VictorModule[] { new VictorModule(4),
+			new VictorModule(5), new VictorModule(6) });
+	
+	VictorModuleGroup right = new VictorModuleGroup(new VictorModule[] { new VictorModule(0),
+			new VictorModule(1), new VictorModule(2) });
+	
+	Drivetrain drivetrain = new Drivetrain(new InversedSpeedController(left), right);
+	
+	Function speedFunction = new Function() {
+		@Override
+		public double F(double in) {
+			return in > 0 ? in * in : -(in * in);
+		}
+	};
+
+	public Subsystem drive = new Subsystem(new Module[] { drivetrain, left, right });
+}
